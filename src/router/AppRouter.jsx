b6540bf585897baac4f0332831c52a0e912b2025
Ravi1470 <vk8580318@gmail.com>
2025-11-29 13:59:07 +0530

    feat: dashboard design completed

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,17 +1,28 @@
 import Login from "../pages/auth/Login";
 import { Route, Routes } from "react-router-dom";
-import HabitForm from "../pages/forms/HabitForm";
+import AllHabitsPage from "../components/habits/AllHabitsPage";
+import Dashboard from "../pages/dashboard/Dashboard";
 
 Routes;
 const AppRouter = () => {
-	return (
-		<div>
-			<Routes>
-				<Route element={<Login />} path="/login" />
-				<Route path="/" element={<HabitForm /> } />
-			</Routes>
-		</div>
-	);
+  return (
+    <div>
+      <Routes>
+        <Route
+          element={<Login />}
+          path="/login"
+        />
+        <Route
+          element={<AllHabitsPage />}
+          path="/Allhabit"
+        />
+        <Route
+          element={<Dashboard />}
+          path="/dashboard"
+        />
+      </Routes>
+    </div>
+  );
 };
 
 export default AppRouter;
